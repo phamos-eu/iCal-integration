003fabafcf2ab52c765236d179d1fae03570063b
pawan <pawan@openetech.com>
2021-02-23 13:03:19 +0530

    add doctype arg

--- icalfeed/icalfeed/utils/calendar.py
+++ icalfeed/icalfeed/utils/calendar.py
@@ -9,37 +9,49 @@ from datetime import datetime
 import frappe
 
 def get_calendar(secret):
-    # check access
-    enabled = frappe.db.get_value("iCal Feed Settings", "iCal Feed Settings", "enabled")
-    if float(enabled) == 0:
-        return
-    erp_secret = frappe.db.get_value("iCal Feed Settings", "iCal Feed Settings", "secret")
-    if not secret == erp_secret:
-        return
+	# initialise calendar
+	cal = Calendar()
+	# set properties
+	cal.add('prodid', '-//iCalFeed module//libracore//')
+	cal.add('version', '2.0')
 
-    # initialise calendar
-    cal = Calendar()
+	event_sub_list = frappe.db.sql("""select
+								doctype_name
+							from
+								`tabiCal Subscription Documents`
+							where
+								`parent` = %s""",(secret), as_dict=1,debug=1)
 
-    # set properties
-    cal.add('prodid', '-//iCalFeed module//libracore//')
-    cal.add('version', '2.0')
+	if event_sub_list:
+		events = frappe.db.sql('''select
+						*
+					from
+						`tabEvent` 
+					where
+						event_type = 'Public' and name in (select parent from `tabEvent Participants` 
+						where reference_doctype in (%s))'''%
+						', '.join(['%s']*len(event_sub_list)), tuple([eve_sub.doctype_name for eve_sub in event_sub_list]), as_dict=1)
+	else:
+		events = frappe.db.sql("""select
+						*
+					from
+						`tabEvent`
+					where
+						event_type = 'Public'""", as_dict=1)
 
-    # get data
-    sql_query = """SELECT * FROM `tabEvent` WHERE `event_type` = 'Public'"""
-    events = frappe.db.sql(sql_query, as_dict=True)
-    # add events
-    for erp_event in events:
-        event = Event()
-        event.add('summary', erp_event['subject'])
-        event.add('dtstart', erp_event['starts_on'])
-        if erp_event['ends_on']:
-            event.add('dtend', erp_event['ends_on'])
-        event.add('dtstamp', erp_event['modified'])
-        event.add('description', erp_event['description'])
-        # add to calendar
-        cal.add_component(event)
+	# add events
+	for erp_event in events:
+		event = Event()
+		event.add('summary', erp_event['subject'])
+		event.add('dtstart', erp_event['starts_on'])
+		if erp_event['ends_on']:
+			event.add('dtend', erp_event['ends_on'])
+		event.add('dtstamp', erp_event['modified'])
+		event.add('description', erp_event['description'])
+		# add to calendar
+		cal.add_component(event)
 
-    return cal
+	return cal
 
 @frappe.whitelist(allow_guest=True)
 def download_calendar(secret):
